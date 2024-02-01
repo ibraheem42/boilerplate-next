dec3462312dbf89955fa655e8455fba8f1bbfdf0
Ibraheem Ahmed <heem4242@gmail.com>
2024-02-01 02:06:55 -0500

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Footer, Header } from '@/components';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Footer />
+    </main>
+  );
+}
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.scss'
+
+export function Footer() {
+  return (
+    <div className={styles.footer}>
+      Contact Us.
+    </div>
+  )
+}
